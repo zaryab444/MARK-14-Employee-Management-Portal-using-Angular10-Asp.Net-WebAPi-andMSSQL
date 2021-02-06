92476bf6b0f5506e50b218f1b52d3b76319a616a
Zaryab <sohail.zaryab61@gmail.com>
2021-02-06 01:51:59 -0800

    initial commit

---
+++ Employee-Management-Portal/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Employee_Management_Portal.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Employee-Management-Portal/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Employee_Management_Portal.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
